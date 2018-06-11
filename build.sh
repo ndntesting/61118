1b360b63986128185561864787939525cf523794
ndntesting <ndntesting@outlook.com>
2018-06-11 18:55:54 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/61118/*.csproj && dotnet build src/61118/*.csproj
---
+++ src/61118/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace _61118
+{
+    public class Class1
+    {
+    }
+}
